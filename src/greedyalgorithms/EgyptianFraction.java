aa9689a189807e34734d191542e9a907396224c8
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2020-07-21 13:19:42 +0530

    Egyptian Fraction Problem - Greedy Method

---
+++ src/greedyalgorithms/EgyptianFraction.java
@@ -0,0 +1,39 @@
+package greedyalgorithms;
+
+public class EgyptianFraction {
+
+	public static void main(String[] args) {
+        int nr = 6, dr = 14; 
+        System.out.print("Egyptian Fraction Representation of " + nr + "/" + dr + " is\n "); 
+        printEgyptian(nr, dr); 
+    } 
+	public static void printEgyptian(int nr, int dr)
+	{
+		if((nr == 0) || (dr == 0))
+		{
+			return;
+		}
+		if(dr%nr == 0)
+		{
+			System.out.print("1/" + dr/nr);
+			return;
+		}
+		
+		if(nr%dr == 0)
+		{
+			System.out.print(nr/dr);
+			return;
+		}
+		
+		if(nr > dr)
+		{
+			System.out.print(nr/dr + " + ");
+			printEgyptian(nr%dr, dr);
+			return;
+		}
+		
+		int n = dr/nr + 1;
+		System.out.print("1/" + n + " + ");
+		printEgyptian(nr*n - dr, dr*n);
+	}
+}
